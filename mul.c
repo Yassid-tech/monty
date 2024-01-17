e52be982e4be2206b4f2dabe1ffdc2eb20f37a8d
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-17 03:59:32 -0800

    mul opcode

---
+++ mul.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * func_mul - performs multiplication on the top two elements of the stack
+ * @stack: stack head
+ * @line_num: line_number
+ * Return: no return
+ */
+void func_mul(stack_t **stack, unsigned int line_num)
+{
+stack_t *current_node;
+int result, node_count;
+current_node = *stack;
+for (node_count = 0; current_node != NULL; node_count++)
+current_node = current_node->next;
+if (node_count < 2)
+{
+fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
+fclose(bus.file);
+free(bus.content);
+free_stack_memory(*stack);
+exit(EXIT_FAILURE);
+}
+current_node = *stack;
+result = current_node->next->n * current_node->n;
+current_node->next->n = result;
+*stack = current_node->next;
+free(current_node);
+}
