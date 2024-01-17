aafa1a9bafca525c86a88b5bd44021c67218067b
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-17 03:11:44 -0800

    pop opcode modif

--- pop.c
+++ pop.c
@@ -9,15 +9,15 @@
 void func_pop(stack_t **stack, unsigned int line_num)
 {
 stack_t *current_node;
-if (*stack_head == NULL)
+if (*stack == NULL)
 {
 fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
 fclose(bus.file);
 free(bus.content);
-free_stack_memory(*stack_head);
+free_stack_memory(*stack);
 exit(EXIT_FAILURE);
 }
-current_node = *stack_head;
-*stack_head = current_node->next;
+current_node = *stack;
+*stack = current_node->next;
 free(current_node);
 }
