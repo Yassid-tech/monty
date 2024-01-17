38b5a44c251bb7954b6d2924ec70546f8b0a26c9
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-01-17 13:49:07 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,14 +1,14 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
  * execute_opcode - executes the opcode
- * @line_content: line content
+ * @content: line content
  * @stack_head: head linked list - stack
  * @line_counter: line_counter
- * @monty_file: pointer to monty file
+ * @file: pointer to monty file
  * Return: no return
 */
-int execute_opcode(char *line_content, stack_t **stack_head, unsigned int line_counter, FILE *monty_file)
+int execute_opcode(char *content, stack_t **stack_head, unsigned int line_counter,FILE *file)
 {
 instruction_t opcode_table[] = {
 {"push", func_push}, {"pall", func_pall}, {"pint", func_pint},
@@ -25,26 +25,26 @@ instruction_t opcode_table[] = {
 };
 unsigned int index = 0;
 char *operation;
-operation = strtok(line_content, " \n\t");
+operation = strtok(content, " \n\t");
 if (operation && operation[0] == '#')
-return 0;
+return (0);
 bus.arg = strtok(NULL, " \n\t");
 while (opcode_table[index].opcode && operation)
 {
 if (strcmp(operation, opcode_table[index].opcode) == 0)
 {
 opcode_table[index].f(stack_head, line_counter);
-return 0;
+return (0);
 }
 index++;
 }
 if (operation && opcode_table[index].opcode == NULL)
 {
 fprintf(stderr, "L%d: unknown instruction %s\n", line_counter, operation);
-fclose(monty_file);
-free(line_content);
+fclose(file);
+free(content);
 free_stack_memory(*stack_head);
 exit(EXIT_FAILURE);
 }
-return 1;
+return (1);
 }
