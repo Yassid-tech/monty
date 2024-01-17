11014a2783f95ead713b2f0bc0c7c5ef9f474dd0
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-17 02:57:34 -0800

    pint opcode and free stack function

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * free_stack_memory - frees a doubly linked list
+ * @stack_head: head of the stack
+ */
+void free_stack_memory(stack_t *stack_head)
+{
+stack_t *current_node;
+current_node = stack_head;
+while (stack_head)
+{
+current_node = stack_head->next;
+free(stack_head);
+stack_head = current_node;
+}
+}
