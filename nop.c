7e5dec96d45fdf45d88a1f5e4eb22891078e77ab
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-17 03:19:39 -0800

    nop opcode

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * func_nop - does nothing (no-operation)
+ * @stack: stack head
+ * @line_num: line_number
+ * Return: no return
+ */
+void func_nop(stack_t **stack, unsigned int line_num)
+{
+(void)line_num;
+(void)stack;
+}
