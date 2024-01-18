a2420361aefb5c3d6d8f1fb9c4c14f8a40f9126a
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-01-18 12:08:33 +0100

    The main.c file

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
 #include "monty.h"
-bus_t my_bus = {NULL, NULL, NULL, 0};
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
  * main - Monty code interpreter
  * @argc: number of arguments
@@ -9,7 +9,6 @@ bus_t my_bus = {NULL, NULL, NULL, 0};
  */
 int main(int argc, char *argv[])
 {
-bus = &my_bus;
 char *line_content;
 FILE *monty_file;
 size_t size = 0;
@@ -22,7 +21,7 @@ fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
 monty_file = fopen(argv[1], "r");
-bus->file = monty_file;
+bus.file = monty_file;
 if (!monty_file)
 {
 fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -32,7 +31,7 @@ while (read_line > 0)
 {
 line_content = NULL;
 read_line = getline(&line_content, &size, monty_file);
-bus->content = line_content;
+bus.content = line_content;
 line_counter++;
 if (read_line > 0)
 {
