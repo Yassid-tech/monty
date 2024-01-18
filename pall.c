b4beecd67ec2acd66bb977d9eddefe464e8259af
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-01-18 12:30:55 -0800

    last modification of the push and pall opcodes

--- pall.c
+++ pall.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * func_pall - performs the print of the stack
- * @stack: stack head
- * @line_num: number's line
- * Return: no return
-*/
+  * func_pall - performs the print of the stack
+  * @stack: stack head
+  * @line_num: number's line
+  * Return: no return
+  */
 void func_pall(stack_t **stack, unsigned int line_num)
 {
 stack_t *current_node;
--- push.c
+++ push.c
@@ -1,24 +1,24 @@
 #include "monty.h"
 
 /**
- * func_push - performs the addition of a node to the stack
- * @stack: stack head
- * @line_num: number's line
- * Return: no return
-*/
+  * func_push - performs the addition of a node to the stack
+  * @stack: stack head
+  * @line_num: number's line
+  * Return: no return
+  */
 void func_push(stack_t **stack, unsigned int line_num)
 {
 int value, index = 0, invalid_input = 0;
-if (yass.arg)
+if(yass.arg)
 {
-if (yass.arg[0] == '-')
+if(yass.arg[0] == '-')
 index++;
-for (; yass.arg[index] != '\0'; index++)
+for(; yass.arg[index] != '\0'; index++)
 {
-if (yass.arg[index] > 57 || yass.arg[index] < 48)
+if(yass.arg[index] > 57 || yass.arg[index] < 48)
 invalid_input = 1;
 }
-if (invalid_input == 1)
+if(invalid_input == 1)
 {
 fprintf(stderr, "L%d: usage: push integer\n", line_num);
 fclose(yass.file);
@@ -36,7 +36,7 @@ free_stack_memory(*stack);
 exit(EXIT_FAILURE);
 }
 value = atoi(yass.arg);
-if (yass.lifi == 0)
+if(yass.lifi == 0)
 add_node(stack, value);
 else
 add_queue(stack, value);
