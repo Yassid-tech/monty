a177aa1714966edec5ac380ee928171325c0c785
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-01-18 17:49:50 +0100

    nop opcode

--- nop.c
+++ nop.c
@@ -3,7 +3,7 @@
 /**
  * func_nop - does nothing (no-operation)
  * @stack: stack head
- * @line_num: line_number
+ * @line_num: number's line
  * Return: no return
  */
 void func_nop(stack_t **stack, unsigned int line_num)
