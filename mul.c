3e9553f703328416e83c67326d8d91834d7e34ca
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-01-18 13:08:13 +0100

    mul opcode

--- mul.c
+++ mul.c
@@ -15,8 +15,8 @@ current_node = current_node->next;
 if (node_count < 2)
 {
 fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
-fclose(bus.file);
-free(bus.content);
+fclose(yass.file);
+free(yass.content);
 free_stack_memory(*stack);
 exit(EXIT_FAILURE);
 }
